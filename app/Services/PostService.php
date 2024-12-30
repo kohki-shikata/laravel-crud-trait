2e71b4aca9c5eafdafd0f495b407ede495fc8e5c
Kohki SHIKATA <indigoworks@gmail.com>
2024-12-31 01:14:21 +0900

    All of methods are tested with Tinker manually

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -40,4 +40,49 @@ class PostService
     {
         return $this->removeCrud($id); // CrudTraitのremoveメソッドを呼び出し
     }
+
+
+    /**
+     * 複数レコードの一括作成
+     *
+     * @param  array $records 作成するレコードの配列
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\View\View レスポンス
+     */
+    public function bulkCreate(array $records)
+    {
+        return $this->bulkCreateCrud($records);
+    }
+
+    /**
+     * 複数同時更新
+     *
+     * @param  array $updates 更新するデータの配列
+     * @return bool 更新成功の場合はtrue、それ以外はfalse
+     */
+    public function bulkUpdate(array $updates): bool
+    {
+        return $this->bulkUpdateCrud($updates);
+    }
+
+    /**
+     * 複数同時削除
+     *
+     * @param  array $ids 削除するモデルのID配列
+     * @return bool 削除成功の場合はtrue、それ以外はfalse
+     */
+    public function bulkDelete(array $ids): bool
+    {
+        return $this->bulkDeleteCrud($ids);
+    }
+
+    /**
+     * 複数カラムで文字列検索
+     *
+     * @param  Request $request 検索リクエスト
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function search($searchTerm, array $columns = ['title', 'description'])
+    {
+        return $this->searchCrud(new \Illuminate\Http\Request(['search' => $searchTerm, 'columns' => $columns]));
+    }
 }
